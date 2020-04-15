e5bca112d25269083f0ca8c1c4a22d92ec8cf5a9
Shubhammankar <shubham.mankar97@gmail.com>
2020-04-15 11:22:26 +0530

    15-04-2020

---
+++ my-login-app/client/authapp/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<style>
+.register .container form .mb-5, .register .container form .my-5 {
+    margin-bottom: 2rem !important;
+}
+</style>
---
+++ my-login-app/client/authapp/src/components/auth/register.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 offset-lg-3 col-sm-10 offset-sm-1">
+        <form
+          class="text-center border border-primary p-5"
+          style="margin-top:70px;height:auto;padding-top:50px !important;"
+          @submit.prevent="registerUser"
+        >
+        <h3 class="heading">Register Form</h3>
+          <input
+            type="text"
+            id="name"
+            class="form-control mb-5"
+            placeholder="First Name"
+            v-model="register.firstname"
+            required
+          />
+                    <input
+            type="text"
+            id="name"
+            class="form-control mb-5"
+            placeholder="Last Name"
+            v-model="register.lastname"
+            required
+          />
+          <input
+            type="email"
+            id="email"
+            class="form-control mb-5"
+            placeholder="Email"
+            v-model="register.email"
+            required
+          />
+
+          <!-- Password -->
+          <input
+            type="password"
+            id="password"
+            class="form-control mb-5"
+            placeholder="Password"
+            v-model="register.password"
+          />
+          <p>
+            Already have an account??<router-link to="/"
+              >click here</router-link
+            >
+
+            <!-- Sign in button -->
+            <center>
+              <button class="btn btn-primary btn-block w-75 my-4" type="submit">
+                Sign in
+              </button>
+            </center>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import swal from "sweetalert";
+export default {
+  data() {
+    return {
+      register: {
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    async registerUser() {
+      try {
+        let response = await this.$http.post("/user/register", this.register);
+        console.log(response);
+        let token = response.data.token;
+        if (token) {
+          localStorage.setItem("jwt", token);
+          this.$router.push("/");
+          swal("Success", "Registration Was successful", "success");
+        } else {
+          swal("Error", "Something Went Wrong", "Error");
+        }
+      } catch (err) {
+        let error = err.response;
+        if (error.status == 409) {
+          swal("Error", error.data.message, "error");
+        } else {
+          swal("Error", error.data.err.message, "error");
+        }
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.heading{
+  text-align: center;
+  padding-bottom: 50px;
+}
+</style>
